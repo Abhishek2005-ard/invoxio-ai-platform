4200e8ddf6771329986da0d0d7dc3793d0572123
Abhishek Dhatrak <dhatraka77@gmail.com>
2026-07-10 21:55:01 +0530

    resolve hardcoded COR

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,9 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
+  origin: process.env.CORS_ORIGIN 
+    ? process.env.CORS_ORIGIN.split(',') 
+    : ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
   credentials: true,
 }));
 app.use(express.json());
